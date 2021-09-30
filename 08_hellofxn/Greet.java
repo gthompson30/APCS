1e67273693559a5d67d4a45ff0a636be67461b17
Gabriel Thompson <gthompson30@stuy.edu>
2021-09-30 16:37:00 -0400

    added part of hw08

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,27 @@
+/*
+Team 10 (Gabriel Thompson, Emily Ortiz, Jonathan Song)
+APCS
+HW08 -- Refactor Freshie Zero
+2021-09-30
+*/
+
+/*
+
+DISCOVERIES:
+
+
+UNRESOLVED QUESTIONS:
+
+*/
+
+public class Greet {
+	public static void main(String[] args) {
+		greet("Gabriel");
+		greet("Emily");
+		greet("Jonathan");
+	}
+	
+	public static void greet(String name) {
+		System.out.println("Why, hello there, " + name + ". How do you do?");
+	}
+}
