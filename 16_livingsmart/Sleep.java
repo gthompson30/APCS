8de24187550bac1225420ae0c1fb90d66d93c04d
Gabriel Thompson <gthompson30@stuy.edu>
2021-10-12 12:15:30 -0400

    added that i sleeped

---
+++ 16_livingsmart/Sleep.java
@@ -0,0 +1,12 @@
+/*
+Gabriel Thompson + Iggy
+APCS
+HW16 -- Living Smart
+2021-10-08 -> 2021-10-12
+*/
+
+public class Sleep {
+	public static void main( String[] args ) {
+		System.out.println("i slepped");
+	}
+}
