b037cc82239d0c74c9aa3238c389d5b90a1fd540
Gabriel Thompson <gthompson30@stuy.edu>
2021-10-18 20:27:42 -0400

    added hw19 OwO

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -1,29 +1,26 @@
 /*
 Nacho Patcho (Gabriel Thompson, Faiza Huda, Jacob Ng)
 APCS
-HW18 -- CPA-One
-2021-10-14 -> 2021-10-16
+HW19 -- Mo Money Mo Problems ...MORE AWESOME
+2021-10-18
 */
 
 /*
 
 DISCO:
- - You can create an instance of a class inside of that same class
- - You can use printf() to print formatted strings. We had done this briefly in the summer homework, but
-   had forgotten how to use it
+ - If you want to make an argument to a function be of the "short" type, it must be manually cast to "short"
+ - We discovered that we don't need a constructor for this asssignment
+ - As well as writing methods to set things like the password, you can also just run something like
+   ba.accPassword = "password123", and this will set the variable.
 
 QCC:
- - Should the ID and PIN number be stored as numbers, or as a String? A number is easier to manage when it
-   comes to verifying that it is valid and within a range, but String preserves leading zeros in IDs
- - In the withdraw() and deposit() functions, should we print out or return the new balance?
-
-Team Nacho Patcho’s Latest and Greatest Q2 Response:
- - Because even without explicitly creating a default constructor, Java is able to create instances of the
-   class
-
-Team Nacho Patcho’s Latest and Greatest Q3 Response:
- - You can try passing the class instance through System.out.println(), and see if it returns a String
-   representation of the object, the same way that it would for ints, floats, Strings, etc.
+ - How can you do formatted strings (like printf) without printing? i.e. how could you apply the same formatting to
+   a string literal, or a variable, without using System.out.printf?
+ - Is there an easier way to accept an input value of the short primitive type, without writing it as an integer
+   literal and casting to short?
+ - What is the purpose of a mutator? Is it just a colloquial term for a method that changes the value of a variable
+   and returns the old value of the changed variable?
+ - What's the point of writing a setPassword, setID, etc. method when you could just do ba.accPassword = "password"?
 
 */
 
@@ -32,77 +29,100 @@ public class BankAccount {
 	private String accHolderName;
 	private String accPassword;
 	private double balance;
-	private int pin;
+	private short pin;
 	private int accID;
-	
+
 	// default constructor, which sets blank values for all instance variables (if no params are given)
 	public BankAccount() {
 		accHolderName = "";
 		accPassword = "";
 		balance = 0.0;
-		pin = 0;
+		pin = (short) 0;
 		accID = 0;
 	}
-	
-	// over-loaded constructor, for if params are given
-	public BankAccount( int id, String name, String pwd, int pinNum ) {
-		// checks to see if id is within valid range, or else yields error message
-		if (0 <= id && id < 1000000000) {
+
+	public String setName( String name ) {
+		accHolderName = name;
+		return name;
+	}
+
+	public void setID( int id ) {
+		if (100000000 <= id && id <= 999999998) {
 			accID = id;
 		} else {
-			System.out.println("Error: ID (" + id + ") not a valid 9-digit number");
-			return;
+			accID = 999999999;
+			System.out.println("Error: ID (" + id + ") not a valid 9-digit number. The ID is now 999999999");
 		}
-		
-		// checks to see if pin is within valid range, or else yields error message
-		if (0 <= pinNum && pinNum < 10000) {
+	}
+
+	public void setBalance( int bal ) {
+		balance = bal;
+	}
+
+	public void setPassword( String pwd ) {
+		accPassword = pwd;
+	}
+
+	public void setPIN( short pinNum ) {
+		if ((short) 1000 <= pinNum && pinNum <= (short) 9998) {
 			pin = pinNum;
 		} else {
-			System.out.println("Error: PIN (" + pinNum + ") not a valid 4-digit number");
-			return;
+			pin = 9999;
+			System.out.println("Error: PIN (" + pinNum + ") is not a valid 4-digit number. The PIN is now 9999");
 		}
-		
-		// sets all other parameters unconditionally
-		accHolderName = name;
-		accPassword = pwd;
-		pin = pinNum;
 	}
-	
+
 	// prints out values of all instance variables. we added extra whitespace so everything aligns.
-	public void printInfo() {
-		System.out.println("======================="); // divider to distinguish between other text
-		System.out.println("Account holder name: " + accHolderName);
-                System.out.println("Account password:    " + accPassword);
-                System.out.printf("Account balance:     %.2f\n", balance);
-                System.out.println("PIN number:          " + pin);
-                System.out.println("Account ID:          " + accID);
-		System.out.println("=======================");
+	public String toString() {
+		String output = "";
+		output += "=======================\n"; // divider to distinguish between other text
+		output += "Account holder name: " + accHolderName + "\n";
+                output += "Account password:    " + accPassword + "\n";
+                output += "Account balance:     " + balance + "\n";
+                output += "PIN number:          " + pin + "\n";
+                output += "Account ID:          " + accID + "\n";
+		output += "=======================\n";
+		return output;
 	}
-	
+
 	// function to add a given amount of money to the balance
-	public double deposit( double amount ) {
+	public void deposit( double amount ) {
 		balance += amount;
-		return balance;
 	}
-	
+
 	// function to remove a given amount of money from the balance
-	public double withdraw( double amount ) {
-		balance -= amount;
-		return balance;
+	public boolean withdraw( double amount ) {
+		if (balance >= amount) {
+			balance -= amount;
+			return true;
+		} else {
+			System.out.printf("Your account does not have enough money ($%.2f) to withdraw $%.2f.\n", balance, amount);
+			return false;
+		}
 	}
-	
-	// TEST CASES
+
+	public boolean authenticate( int id, String password ) {
+		return (id == accID) && (password == accPassword);
+	}
+
+	// TEST CASES, courtesy of team KLEH (pd8). Idea for casting to "short" courtesy of Clueless Cats (pd8)
 	public static void main( String[] args ) {
-		// test with all valid arguments
-                BankAccount myAcc = new BankAccount( 123456789, "Nachos", "password123", 1336 );
-                myAcc.deposit(456);
-                myAcc.withdraw(3);
-                myAcc.printInfo(); // should return account details with balance = 453.00
-		
-		// test with valid pin, not valid id
-		myAcc = new BankAccount( 1000000000, "Nachos", "password123", 1336 ); // should yield an error message about ID
-		
-		// test with valid id, not valid pin
-		myAcc = new BankAccount( 123456789, "Nachos", "password123", 1336433 ); // should yield an error message about ID
-        }
+		BankAccount ba = new BankAccount();
+		ba.setName("A");
+		ba.setPassword("password");
+		ba.setPIN((short) 1234);
+		ba.setID(123456789);
+		ba.setBalance(500);
+		ba.deposit(200);
+		ba.withdraw(100);
+		String accinfo = ba.toString();
+		System.out.println(accinfo);
+		// our test cases for authenticate() and withdraw error messages
+		System.out.println("\nAuthentication tests:");
+		System.out.println("Authentication: " + ba.authenticate(123456789, "password") + " (should be true)");
+		System.out.println("Authentication: " + ba.authenticate(347934573, "bigchungus") + " (should be false)");
+		System.out.println("\nWithdrawal test:");
+		System.out.println(ba.withdraw(1000) + " (should yield error)");
+	}//end main()
+
 }
