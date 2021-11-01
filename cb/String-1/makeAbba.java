de8968f3358000cd7f7ba3b60513b0e02f2100a0
Gabriel Thompson <gthompson30@stuy.edu>
2021-11-01 12:11:05 -0400

    Update makeAbba.java

--- cb/String-1/makeAbba.java
+++ cb/String-1/makeAbba.java
@@ -2,4 +2,9 @@ public class makeAbba {
 	public String makeAbba( String a, String b ) {
 		return a + b + b + a;
 	}
+	
+	public static void main( String[] args ) {
+		System.out.println(makeAbba("Hi", "Bye"));
+		System.out.println(makeAbba("Yo", "Alice"));
+	}
 }
