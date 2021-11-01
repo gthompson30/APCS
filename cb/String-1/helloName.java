f8e283861b1b5d72c6e67104020e89728492d063
Gabriel Thompson <gthompson30@stuy.edu>
2021-11-01 12:10:48 -0400

    Update helloName.java

--- cb/String-1/helloName.java
+++ cb/String-1/helloName.java
@@ -2,4 +2,9 @@ public class helloName {
 	public String helloName( String name ) {
 		return "Hello " + name + "!";
 	}
+	
+	public static void main( String[] args ) {
+		System.out.println(helloName("Bob"));
+		System.out.println(helloName("Alice"));
+	}
 }
