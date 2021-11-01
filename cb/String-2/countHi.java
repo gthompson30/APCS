0c7c8f245f47c8d3695fbb190f6f83bc5ac35ba3
Gabriel Thompson <gthompson30@stuy.edu>
2021-11-01 12:14:52 -0400

    Update countHi.java

--- cb/String-2/countHi.java
+++ cb/String-2/countHi.java
@@ -2,4 +2,9 @@ public class countHi {
 	public int countHi(String str) {
 		return catDog.countInstances(str, "hi");
 	}
+	
+	public static void main( String[] args ) {
+		System.out.println(countHi("hiHIhi") + " ... should be 2");
+		System.out.println(countHi("Hi is no HI on ahI") + " ... should be 0");
+	}
 }
