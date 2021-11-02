cd81b45cb187c6a1cbfa9d9f784b24b7d652e471
Gabriel Thompson <gthompson30@stuy.edu>
2021-11-01 20:32:44 -0400

    Update Commafier.java

--- 29_cliforeastr/Commafier.java
+++ 29_cliforeastr/Commafier.java
@@ -1,13 +1,30 @@
+/*
+Nacho Patcho (Jacob Ng + PreGuac, Gabriel Thompson + Iggy, Faiza Huda + truthfulTom)
+APCS
+HW29 -- Which Way Do You Roll?
+2021-11-01
+time spent: 1.0hrs (at the Dojo)
+*/
+
+/*
+DISCO:
+ - Integer.parseInt() converts a String to an integer
+ - Integer.toString() converts an integer to a String
+
+QCC:
+ - For the function commafyF(), why is there an offset for (strLen - i) % 3 == 0, but rather 1?
+ - Is there a more efficient way to write either the iterative or the recursive function?
+*/
+
 public class Commafier {
-	
 	public static String commafyF( int n ) {
-		String strN = Integer.toString(n);
 		String output = "";
-		int strLen = strN.length();
+		String nStr = Integer.toString(n);
+		int strLen = nStr.length();
 		
 		for (int i = 0; i < strLen; i++) {
-			output += strN.charAt(i);
-			if ((strLen - i) % 3 == 1 && i != strLen - 1) {
+			output += nStr.charAt(i);
+			if ((strLen - i) % 3 == 1 && (i != (strLen - 1))) {
 				output += ",";
 			}
 		}
@@ -15,21 +32,23 @@ public class Commafier {
 	}
 	
 	public static String commafyR( int n ) {
-		String strN = Integer.toString(n);
 		String output = "";
-		int strLen = strN.length();
+                String nStr = Integer.toString(n);
+                int strLen = nStr.length();
 		
-		if (strLen < 3) {
-			return strN;
+		if (strLen <= 3) {
+			return nStr;
 		}
-		else {
-			return commafyR(strN.substring(3, strLen - 1)) + ", " + strN;
-		}
+		return commafyR(Integer.parseInt(nStr.substring(0, strLen-3))) + "," + nStr.substring(strLen -3);
 	}
 	
 	public static void main( String[] args ) {
-		System.out.println(commafyF(2147483647));
-		System.out.println(commafyR(2147483647));
+		// System.out.println(commafyF(476730128));
+		// System.out.println(commafyR(476730128));
+		for ( String arg : args ) {
+			System.out.println(commafyF(Integer.parseInt(arg)));
+			System.out.println(commafyR(Integer.parseInt(arg)));
+		}
+
 	}
-	
 }
