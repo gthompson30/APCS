082c8559c63c0154e7fa7cdf51e0d97e85e0bd4e
Gabriel Thompson <gthompson30@stuy.edu>
2021-12-01 13:25:09 -0500

    finished classwork

--- 41_rat/Rational.java
+++ 41_rat/Rational.java
@@ -11,10 +11,28 @@ public class Rational {
 		if (denom == 0) {
 			numerator = 0;
 			denominator = 1;
-			System.out.println("ERROR");
+			System.out.println("ERROR: Denominator cannot be 0");
 		} else {
 			numerator = num;
 			denominator = denom;
 		}
+	}
+
+	public String toString() {
+		return numerator + "/" + denominator;
+	}
+
+	public float floatValue() {
+		return (float) numerator / denominator;
+	}
+
+	public void multiply(Rational r) {
+		numerator *= r.numerator;
+		denominator *= r.denominator;
+	}
+
+	public void divide(Rational r) {
+		numerator *= r.denominator;
+		denominator *= r.numerator;
 	}
 }
