2defd2bf385a79d5211a5284ce560f646e56e37e
Gabriel Thompson <43486098+Gabriel786432@users.noreply.github.com>
2021-12-05 19:19:17 -0500

    finished hw42

--- 42_rat/Rational.java
+++ 42_rat/Rational.java
@@ -1,27 +1,30 @@
 /***
 discordDucks (Gabriel Thompson + Iggy, Kartik Vanjani + Krrish, Marcus Wu + Nat)
 APCS pd8
-HW41 -- Be Rational
-2021-12-01
-time spent: 0.5hrs
+HW42 -- Be More Rational
+2021-12-05
+time spent: 0.6hrs
 ***/
 
 /***
 
 DISCO:
- - "double" is a more accurate way of storing floating-point values, and therefore is our returntype of
-   choice for the function floatValue()
- - You need to check if the resulting denominator will be zero for both multiply() and divide(), to
-   make sure that each method doesn't set the ratio the class stores to an invalid fraction
+ - gcd() doesn't take in two inputs as we previously thought, rather, it uses the numerator and
+   denominator of the class instance
+ - You can subtract by just negating the parameter instance, and running add() with it
+ - You can write if statements without {} brackets, and also not use brackets for following else if
+   and else statements
+ - We didn't need to make 30 separate variables for the test cases, as we could've reassigned the
+   values of them, but we only realized this at the end and didn't want to change all of them
 
 QCC:
- - Is there a better way to handle making sure that you don't divide by 0? It makes the code more ugly
-   because the entire function has to be within an if/else statement
- - What is the harm of making instance variables (particularily the ones in this case) public, what is
-   the advantage of keeping them private?
- - It seems inefficient to create so many instances of "Rational" for the test cases -- is there a
-   cleaner or more efficient way of doing that?
- - Does Java have a built in ratio object similar to this one?
+ - Is there anyone to write add() without temporarily storing the other denominator? It's not that
+   bad but it makes it a bit less concise
+ - Our code doesn't fully work with negative numerators or denominators, because we can't run GCD
+   on them. We're not sure how to fix this
+ - We're not totally sure that we did the right thing in returning either 1, 0, or -1 for compareTo().
+   The instructions just said to return "a positive integer" or "a negative integer" or 0, but I
+   this feels sort of vague somehow
 
 ***/
 
@@ -73,26 +76,43 @@ public class Rational {
 	}
 
 	public void add( Rational r ) {
+		int tempDenominator = this.denominator;
 		this.numerator = this.numerator * r.denominator;
 		this.denominator = this.denominator * r.denominator;
-		// Unnecessary but why not
-		reduce();
+		r.denominator = r.denominator * tempDenominator;
+		r.numerator = r.numerator * tempDenominator;
+
+		this.numerator = this.numerator + r.numerator;
 	}
 
-	public int gcd( int a, int b ){
+	public void subtract( Rational r ) {
+		// multiply other by -1, then add
+		r.multiply( new Rational(-1, 1) );
+		add(r);
+	}
+
+	public int gcd(){
                 int i = 1;
                 int answer = 0;
-                while (i <= a){
-                        if (a % i == 0 && b % i == 0)   {answer = i;}
+                while (i <= this.numerator){
+                        if (this.numerator % i == 0 && this.denominator % i == 0)   {answer = i;}
                         i++;
                 }
                 return answer;
         }
 
 	public void reduce() {
-		int divisor = gcd(this.numerator, this.denominator);
+		int divisor = gcd();
 		this.numerator = this.numerator / divisor;
 		this.denominator = this.denominator / divisor;
+	}
+
+	public int compareTo( Rational r ) {
+		if (this.floatValue() > r.floatValue())
+			return 1;
+		else if (this.floatValue() < r.floatValue())
+			return -1;
+		return 0;
 	}
 
 	public static void main(String[] args) {
@@ -131,5 +151,48 @@ public class Rational {
 		Rational n = new Rational();
 		m.divide(n);
 		System.out.println(m + " ...should yield an error\n");
+
+		System.out.println("\nadd() test cases:");
+		Rational o = new Rational(1, 2);
+		Rational p = new Rational(3, 4);
+		o.add(p);
+		System.out.println(o + " ...should be equivalent to \"5 / 4\"");
+		Rational q = new Rational(5, 7);
+		Rational r = new Rational(1, 2);
+		q.add(r);
+		System.out.println(q + " ...should be equivalent to \"17 / 14\"");
+
+                System.out.println("\nsubtract() test cases:");
+                Rational s = new Rational(1, 2);
+                Rational t = new Rational(3, 4);
+                s.subtract(t);
+                System.out.println(s + " ...should be equivalent to \"-1 / 4\"");
+                Rational u = new Rational(5, 7);
+                Rational v = new Rational(1, 2);
+                u.subtract(v);
+                System.out.println(u + " ...should be equivalent to \"3 / 14\"");
+
+		System.out.println("\ngcd() test cases:");
+		Rational w = new Rational(8, 4);
+		System.out.println(w.gcd() + " ...should be \"4\"");
+                Rational x = new Rational(7, 3);
+                System.out.println(x.gcd() + " ...should be \"1\"");
+
+		System.out.println("\nreduce() test cases:");
+		w.reduce();
+                System.out.println(w + " ...should be \"2 / 1\"");
+		x.reduce();
+                System.out.println(x + " ...should be \"7 / 3\"");
+
+		System.out.println("\ncompareTo() test cases:");
+		Rational y = new Rational(51, 17);
+		Rational z = new Rational(48, 16);
+		System.out.println(y.compareTo(z) + " ...should be \"0\"");
+		Rational aa = new Rational(6, 3);
+		Rational ab = new Rational(23, 12);
+		System.out.println(aa.compareTo(ab) + " ...should be \"1\"");
+		Rational ac = new Rational(4, 3);
+		Rational ad = new Rational(3, 2);
+		System.out.println(ac.compareTo(ad) + " ...should be \"-1\"");
 	}
 }
