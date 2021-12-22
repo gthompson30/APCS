89a14a2ef345072d30b3165df044cedc360896b0
Gabriel Thompson <gthompson30@stuy.edu>
2021-12-22 14:06:09 -0500

    lol

---
+++ 49_rat/Driver.java
@@ -0,0 +1,13 @@
+/***
+discordDucks (Gabriel Thompson + Iggy, Kartik Vanjani + Krrish, Marcus Wu + Nat)
+APCS pd8
+HW49 -- Rational Standards Compliance
+2021-12-22
+time spent: X.Xhrs
+***/
+
+public class Driver {
+
+
+
+}
