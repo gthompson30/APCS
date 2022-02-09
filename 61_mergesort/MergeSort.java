b82ebc53e63d4878926ff8c8d2d83e9da0d55223
Gabriel Thompson <gthompson30@stuy.edu>
2022-02-08 20:57:55 -0500

    finished hw61

--- 61_mergesort/MergeSort.java
+++ 61_mergesort/MergeSort.java
@@ -2,7 +2,30 @@
   class MergeSort
   Implements mergesort on array of ints.
 
-  Summary of Algorithm:
+  Summary of Sort Algorithm:
+     I. Is the inputted array of length 1?
+         a. Yes!
+            -> Return the array, because arrays of length 1 are always sorted.
+         b. No!
+            -> Continue to step II
+    II. Calculate the middle index of the array
+   III. Create two separate arrays, each of which represent the left and right part of the input array, spliced at the calculated middle index.
+    IV. Call the sort() function on each of the halves, to sort each half.
+     V. Merge the two halves, through the "Merge" algorithm.
+    VI. Return the merged halves. This is the sorted array.
+
+  Summary of Merge Algorithm:
+     I. Create an empty output array.
+    II. Initialize two variables, aPos and bPos, to keep track of the current position we're looking at in each array. Both aPos and bPos are set to 0.
+   III. Repeat the following until aPos reaches the end index of the first array or bPos reaches the end index of the second array:
+         a. Compare the value in the first array at aPos with the value in the second array at bPos.
+         b. Is the value at aPos is smaller than the value at bPos?
+            i. Yes!
+               -> add the value at aPos to the output array. Increment aPos by 1.
+           ii. No!
+               -> add the value at bPos to the output array. Increment bPos by 1.
+    IV. Once you've finished adding all of the elements from either of the arrays, add all unadded elements to the end of the output array.
+     V. Return the output array.
 
   ***/
 
@@ -40,11 +63,11 @@ public class MergeSort
      }
 
      for (int i = aPos; i < a.length; i++) {
-       output[addedToOutput] = a[aPos];
+       output[addedToOutput] = a[i];
        addedToOutput++;
      }
      for (int i = bPos; i < b.length; i++) {
-       output[addedToOutput] = b[bPos];
+       output[addedToOutput] = b[i];
        addedToOutput++;
      }
 
@@ -65,12 +88,16 @@ public class MergeSort
     int midPoint = arr.length / 2;
     int[] firstHalf = new int[midPoint];
     int[] secondHalf = new int[arr.length - midPoint];
-    for (int i = 0; i < firstHalf; i++) {
+    for (int i = 0; i < midPoint; i++) {
       firstHalf[i] = arr[i];
     }
     for (int i = 0; i < secondHalf.length; i++) {
       secondHalf[i] = arr[i + midPoint];
     }
+    firstHalf = sort(firstHalf);
+    secondHalf = sort(secondHalf);
+
+    return merge(firstHalf, secondHalf);
   }//end sort()
 
 
@@ -116,12 +143,27 @@ public class MergeSort
       System.out.println("\nMerging arr4 and arr6: ");
       printArray( merge(arr4,arr6) );
 
-    /*~~~~~~~~~~~~~~ Ye Olde Tester Bar ~~~~~~~~~~~~~~
       System.out.println("\nSorting arr4-7...");
       printArray( sort( arr4 ) );
       printArray( sort( arr5 ) );
       printArray( sort( arr6 ) );
       printArray( sort( arr7 ) );
+
+      // TEAM TAME MEAT MATE META SPEED TEST
+      int[] arr8 = new int[1000000];
+      for (int i = 0 ; i < arr8.length ; i++)
+        arr8[i] = (int) (Math.random() * 10000000);
+
+      long startTime = System.currentTimeMillis();
+      int[] sortedArr8 = sort( arr8 );
+      long endTime = System.currentTimeMillis();
+      int[] firstSample = new int[1000];
+      for (int i = 0; i < firstSample.length; i++)
+        firstSample[i] = sortedArr8[i];
+      printArray(firstSample);
+
+      System.out.println("SORTED! In only " + (endTime - startTime) + "ms.");
+    /*~~~~~~~~~~~~~~ Ye Olde Tester Bar ~~~~~~~~~~~~~~
       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
   }//end main()
 
