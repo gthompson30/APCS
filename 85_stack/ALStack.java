50be4aebc555bf526e2d3c47c67509f48b2c6d13
Gabriel Thompson <gthompson30@stuy.edu>
2022-03-30 19:27:36 -0400

    work so far

---
+++ 85_stack/ALStack.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class ALStack<PANCAKE> implements Stack {
+
+	ArrayList<PANCAKE> stack;
+
+	public ALStack() {
+		stack = new ArrayList<PANCAKE>();
+	}
+
+	public boolean isEmpty() {
+		return this.stack.size() == 0;
+	}
+
+	public PANCAKE peekTop() {
+		if (isEmpty()) return null;
+		return this.stack.get(this.stack.size() - 1);
+	}
+
+	public PANCAKE pop() {
+		PANCAKE lastItem = peekTop();
+		this.stack.remove(this.stack.size() - 1);
+		return lastItem;
+	}
+
+	public void push( PANCAKE x ) {
+		this.stack.add(x);
+	}
+
+}
+
