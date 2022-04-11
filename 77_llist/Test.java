7e3ee7a353615f816da46be5541a22442f1c4426
Gabriel Thompson <gthompson30@stuy.edu>
2022-04-11 13:27:20 -0400

    work

---
+++ 77_llist/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+	public static void main(String[] args) {
+		List a = new LList();
+		a.add("Hi");
+		System.out.println(a);
+		a.add("there");
+		System.out.println(a);
+	}
+
+}
