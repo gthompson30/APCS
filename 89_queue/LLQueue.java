46e57ccaba284acd00ae38d38f83de326f115c31
Gabriel Thompson <gthompson30@stuy.edu>
2022-04-11 20:25:13 -0400

    Update LLQueue.java

--- 89_queue/LLQueue.java
+++ 89_queue/LLQueue.java
@@ -4,6 +4,16 @@
 // 2022-04-11
 // time spent: 0.3hrs
 
+/***
+ * DISCO
+ *  - Nodes need not be involved in a Queue, only through a LinkedList
+ *  - All LLQueue methods are constant time
+ *  - When using a method like removeFirst() or getFirst() which returns the cargo at one node, you must typecast the output to the generic type.
+ * 
+ * QCC
+ *  - Why must we typecast such as at line 29?
+ ***/
+
 import java.util.LinkedList;
 
 public class LLQueue<T> implements Queue<T> {
@@ -33,5 +43,16 @@ public class LLQueue<T> implements Queue<T> {
 	public T peekFront() {
 		return (T) this.queue.getFirst();
 	}
-
+	
+	public static void main(String[] args) {
+		LLQueue<String> q = new LLQueue<String>();
+		q.enqueue("Hello");
+		q.enqueue("There");
+		System.out.println(q.peekFront()); // "Hello"
+		q.dequeue();
+		System.out.println(q.isEmpty());   // false
+		System.out.println(q.peekFront()); // "There"
+		System.out.println(q.isEmpty());   // true
+	}
+	
 }
