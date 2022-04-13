f17ac376345a89440dc42bfa9ed82407482b0063
Gabriel Thompson <gthompson30@stuy.edu>
2022-04-13 07:51:50 -0400

    Update Deque.java

--- 90_deque/Deque.java
+++ 90_deque/Deque.java
@@ -1,25 +1,36 @@
 /***
- * interface Queue
+ * interface Deque
  * Includes only methods listed in AP subset of Java
  * (Local version, to override built-in Java implementation.)
  **/
 
-public interface Queue<QUASAR>
+public interface Deque<QUASAR>
 {
   //~~~~~~~~~~~~~~~~~~begin AP subset~~~~~~~~~~~~~~~~~~
   //means of removing an element from collection:
+
   //Dequeues and returns the first element of the queue.
-  public QUASAR dequeue();
+  public QUASAR removeFirst();
+  //Dequeues and returns the last element of the queue.
+  public QUASAR removeLast();
+
 
   //means of adding an element to collection:
+
   //Enqueue an element onto the back of this queue.
-  public void enqueue( QUASAR x );
+  public void addFirst( QUASAR x );
+  //Enqueue an element onto the back of this queue.
+  public void addLast( QUASAR x );
 
   //Returns true if this queue is empty, otherwise returns false.
   public boolean isEmpty();
 
   //Returns the first element of the queue without dequeuing it.
-  public QUASAR peekFront();
+  public QUASAR getFirst();
+  //Returns the last element of the queue without dequeuing it.
+  public QUASAR getLast();
+
+
   //~~~~~~~~~~~~~~~~~~~end AP subset~~~~~~~~~~~~~~~~~~~
 
-}//end interface Queue
+}//end interface Deque
